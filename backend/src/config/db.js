236960b4acfee246f0e2f9d174928e683688e408
swainrakesh990 <swainrakesh990@gmail.com>
2025-09-09 11:21:15 +0000

    db updated

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
+    mongoose.set('strictQuery', true);
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
